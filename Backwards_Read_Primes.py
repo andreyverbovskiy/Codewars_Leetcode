1d6ae72ba230c01135014e5d2feacd44428245cf
andreyverbovskiy <56063258+andreyverbovskiy@users.noreply.github.com>
2021-09-13 14:57:00 +0300

    Create Backwards_Read_Primes.py

---
+++ Backwards_Read_Primes.py
@@ -0,0 +1,32 @@
+def backwards_prime(start, stop):    
+    answ = []
+    answ1 = []
+    
+    for x in range(start,stop+1,1): 
+        arr = []
+        for y in range(1,(x+1)/2,1):
+            if(x%y ==0):
+                arr.append(y)
+        if(len(arr)==2):
+            answ.append(x)               
+    
+    
+    
+    
+    for k in answ:
+        reversed = int(str(k)[::-1])
+        answ1.append(reversed)        
+    answ = []      
+    for s in answ1:
+         
+        arr = []
+        for y in range(1,(s+1)/2,1):
+            if(s%y ==0):
+                arr.append(y)
+        if(len(arr)==2):
+            answ.append(s)    
+    answ1 = []   
+    for k in answ:
+        reversed = int(str(k)[::-1])
+        answ1.append(reversed)        
+    return answ1
